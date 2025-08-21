a4004756607e8e8c4feea4dcab1fb1cbd93780a0
antonshk2023 <antonshk2013@gmail.com>
2025-08-21 11:31:05 +0200

    home work 3

--- home_task3/main.py
+++ home_task3/main.py
@@ -19,43 +19,42 @@ def driver():
 
 
 
-"""Кнопки переключения языка (ru и de)"""
 def test_change_languig(driver):
     start_url = "https://itcareerhub.de/ru"
     driver.get(start_url)
-    sleep(2)
-    link_payment_method = driver.find_element(By.LINK_TEXT, "de")
-    sleep(2)
-    link_payment_method.click()
-    sleep(2)
-    new_url_str = "https://itcareerhub.de/"
-    new_url = driver.current_url
-    assert new_url_str == new_url
 
-"""Открывает https://itcareerhub.de/ru
-Проверяет, что на странице отображаются:
-Логитип ITCareerHub
-Ссылка “Программы”
-Ссылка “Способы оплаты”
-Ссылка “Новости”
-Ссылка “О нас”
-Ссылка “Отзывы”
-Кнопки переключения языка (ru и de)
-Кликнуть по иконке с телефонной трубкой
-Проверить что текст “Если вы не дозвонились, заполните форму на сайте. Мы свяжемся с вами” отображается."""
 
-def test_phone_icon(driver):
-    start_url = "https://itcareerhub.de/ru"
-    driver.get(start_url)
-    sleep(2)
+    """# Проверяет, что на странице отображаются: Логитип ITCareerHub"""
+    assert driver.find_element(By.CSS_SELECTOR, 'img[alt="IT Career Hub"]'), "logo not found"
 
-    selector_string = "#rec717843722 > div > div > div.t396__elem.tn-elem.tn-elem__7178437221710153310161 > a"
-    link_payment_method = driver.find_element(By.CSS_SELECTOR, selector_string)
-    sleep(2)
-    link_payment_method.click()
-    sleep(2)
-    new_url_str = "https://itcareerhub.de/"
-    new_url = driver.current_url
-    assert new_url_str == new_url
+    """# Проверяет, что на странице отображаются: 
+        Ссылка “Программы”
+        Ссылка “Способы оплаты”
+        Ссылка “Новости”
+        Ссылка “О нас”
+        Ссылка “Отзывы”"""
 
-# mg[imgfield="tn_img_1710153310161"]
+    texts = [
+        "Программы",
+        "Способы оплаты",
+        "Новости",
+        "О нас",
+        "Отзывы"
+    ]
+    for text in texts:
+        assert driver.find_element(By.LINK_TEXT, text), f"Link '{text}' not found"
+
+    """# Проверяет, что на странице отображаются: Кнопки переключения языка (ru и de)"""
+    button_lang_de = driver.find_element(By.LINK_TEXT, "de")
+    button_lang_ru = driver.find_element(By.LINK_TEXT, "ru")
+    assert button_lang_de.is_displayed(), "Button 'de' is hidden"
+    assert button_lang_ru.is_displayed(), "Button 'ru' is hidden"
+
+    """# Проверяет, что на странице отображаются: Кнопки переключения языка (ru и de)"""
+    icon = driver.find_element(By.CSS_SELECTOR, 'img[imgfield="tn_img_1710153310161"]')
+    icon.click()
+    driver.implicitly_wait(2)
+
+    """Проверяет, что на странице отображаются Проверить что текст “Если вы не дозвонились, заполните форму на сайте. Мы свяжемся с вами” отображается"""
+    text = "Если вы не дозвонились, заполните форму на сайте. Мы свяжемся с вами"
+    assert text in driver.page_source, "Waiting anothe text"
